df42c98ed0e8dc9d971cf740c0ad31f8c337656d
Emon-git-site <136096636+Emon-git-site@users.noreply.github.com>
2023-09-18 16:42:12 +0600

    Hospital Home page

---
+++ asset/js/custom.js
@@ -0,0 +1,13 @@
+(function($) {
+    $(document).ready(function() {
+        $('.slider-hero').slick({
+            dots: true,
+            // autoplay:true,
+            autoplaySpeed:1000,
+            infinite: true,
+            cssEase: 'linear',
+            swipe: true,
+        });
+    });
+	
+})( jQuery );
