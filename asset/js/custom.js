8ff0ac4b3ab181569bd326748d29f27b1cacc118
Md Alauddin <136096636+Emon-git-site@users.noreply.github.com>
2023-09-19 07:22:03 +0600

    slider auto play true done

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -2,7 +2,7 @@
     $(document).ready(function() {
         $('.slider-hero').slick({
             dots: true,
-            // autoplay:true,
+            autoplay:true,
             autoplaySpeed:1000,
             infinite: true,
             cssEase: 'linear',
